6546bd56409629b0e33f6eb7b06582f7ddc87072
jane308pr <jane308pr.personal@gmail.com>
2025-10-05 21:33:28 +0300

    homework for lesson 5

---
+++ lesson5/src/getter-setters.js
@@ -0,0 +1,42 @@
+const bookStore = {
+    name: 'The best book store ever',
+    _books: [
+        {  title: 'Тіні забутих предків', author: 'Михайло Коцюбинський', price: 250, isAvailable: true },
+        {  title: 'Лісова пісня', author: 'Леся Українка', price: 200, isAvailable: false }
+    ],
+    _address: {
+        city: 'Kyiv',
+        street: 'Vasylkivska'
+    },
+    getAvailableBooks() {
+        return this._books.filter(book => book.isAvailable).map(book => ({
+            bookTitle: book.title,
+            bookAuthor: book.author
+        }));
+    },
+    set book(book) {
+        this._books.push(book);
+    },
+    set address(address) {
+        this._address = address;
+    },
+    get address() {
+        return this._address;
+    },
+    getBookStoreInfo() {
+        return {
+            name: this.name,
+            city: this._address.city,
+            street: this._address.street,
+            availableBooks: this.getAvailableBooks()
+        };
+    }
+
+};
+
+console.log('Book store address:', bookStore.address);
+bookStore.address = { city: 'Kyiv', street:'Khreschatyk' };
+bookStore.book = { title: 'Кайдашева сім’я', author: 'Іван Нечуй-Левицький', price: 230, isAvailable: true };
+console.log('Book store info', bookStore.getBookStoreInfo());
+
+
