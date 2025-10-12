7fa7b8bb6ac5ce0b28b83a6c909b56e9805d210b
jane308pr <jane308pr.persnal@gmail.com>
2025-10-12 16:41:11 +0300

    Homework for Lesson 7

---
+++ lesson7/src/const/arrays.ts
@@ -0,0 +1,5 @@
+export const stringArray: string[] = ['cat', 'monkey', 'dog', 'panda', 'elephant', 'mouse'];
+export const numberArray: number[] = [10, 5, 20, 15, 30];
+export const mixedArray: (string | number)[] = [20, 'cat', 'monkey', 10, 5];
+export const singleString: string[] = ['cat'];
+export const emptyArray: string[] = [];
---
+++ lesson7/src/ts-arrow-functions.ts
@@ -0,0 +1,29 @@
+import { stringArray, numberArray, mixedArray, singleString, emptyArray } from './const/arrays';
+
+const calcArraySum = (array: (string | number)[]): void => {
+    if (array.length === 0) {
+        console.log('Array is empty\n');
+        return;
+    }
+
+    const firstType = typeof array[0];
+    const isHomogeneous = array.every(item => typeof item === firstType);
+    if (!isHomogeneous) {
+        console.log('Array contains mixed types (both string and number)\n');
+        return;
+    }
+
+    if (firstType === 'number') {
+        const sum = (array as number[]).reduce((acc, num) => acc + num, 0);
+        console.log('Sum of array:', sum, '\n');
+    } else {
+        const sum = (array as string[]).join('');
+        console.log('Concatenated string:', sum, '\n');
+    }
+};
+
+calcArraySum(stringArray);
+calcArraySum(numberArray);
+calcArraySum(singleString);
+calcArraySum(mixedArray);
+calcArraySum(emptyArray);
