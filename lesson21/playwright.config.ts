6d60d1a1c98cda4b8a16e19bc5e01208dacc758a
jane308pr <jane308pr.personal@gmail.com>
2025-12-10 20:40:24 +0200

    update all builds

--- lesson21/playwright.config.ts
+++ lesson21/playwright.config.ts
@@ -38,7 +38,11 @@ export default defineConfig({
 
         /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
         trace: 'retain-on-failure',
-        screenshot: 'only-on-failure'
+        screenshot: 'only-on-failure',
+        video: {
+            mode: 'retain-on-failure',
+            size: { width: 1680, height: 900 }
+        }
     },
 
     /* Configure projects for major browsers */
